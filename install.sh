434d706557d902ec3f11cd8166b11721c3263071
Colin Dembovsky <colindembovsky@github.com>
2022-01-04 10:12:58 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,28 @@
+#!/bin/sh
+
+zshrc() {
+    echo "==========================================================="
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "==========================================================="
+    echo "             cloning zsh-syntax-highlighting               "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    echo "==========================================================="
+    echo "             cloning powerlevel10k                         "
+    echo "-----------------------------------------------------------"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    echo "==========================================================="
+    echo "             import zshrc                                  "
+    echo "-----------------------------------------------------------"
+    cat .zshrc > $HOME/.zshrc
+    echo "==========================================================="
+    echo "             import powerlevel10k                          "
+    echo "-----------------------------------------------------------"
+    cat .p10k.zsh > $HOME/.p10k.zsh
+}
+
 zshrc
 
 # these need to be after zshrc
