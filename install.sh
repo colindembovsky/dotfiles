56714e5d171423227f0c6b5c4139276b17ae6302
Colin Dembovsky <colindembovsky@github.com>
2022-01-04 18:24:08 +0000

    Stuff

--- install.sh
+++ install.sh
@@ -17,11 +17,11 @@ zshrc() {
     echo "==========================================================="
     echo "             cloning zsh-autosuggestions                   "
     echo "-----------------------------------------------------------"
-    #git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     echo "==========================================================="
     echo "             cloning zsh-syntax-highlighting               "
     echo "-----------------------------------------------------------"
-    #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     echo "==========================================================="
     echo "             import zshrc                                  "
     echo "-----------------------------------------------------------"
