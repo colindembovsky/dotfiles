23ed499dfd663919f92a0a29efc0a2929023d938
Colin Dembovsky <colindembovsky@github.com>
2022-01-04 17:10:21 +0000

    Stuffs

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 zshrc() {
     echo "==========================================================="
+    echo "             install powerline-shell                       "
+    echo "-----------------------------------------------------------"
+    pip install powerline-shell
+    echo "==========================================================="
     echo "             cloning zsh-autosuggestions                   "
     echo "-----------------------------------------------------------"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -10,17 +14,9 @@ zshrc() {
     echo "-----------------------------------------------------------"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     echo "==========================================================="
-    echo "             cloning powerlevel10k                         "
-    echo "-----------------------------------------------------------"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    echo "==========================================================="
     echo "             import zshrc                                  "
     echo "-----------------------------------------------------------"
     cat .zshrc > $HOME/.zshrc
-    echo "==========================================================="
-    echo "             import powerlevel10k                          "
-    echo "-----------------------------------------------------------"
-    cat .p10k.zsh > $HOME/.p10k.zsh
 }
 
 zshrc
